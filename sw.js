cc0eebff3425948d8fddfe99a31732fdaa2feeeb
lasierra.tae <lasierra.tae@gmail.com>
2026-03-02 08:08:05 +0000

    Fix PWA launch issue: use relative paths in manifest and service worker

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
-const CACHE_NAME = 'landinfo-v1';
+const CACHE_NAME = 'landinfo-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
-  '/icon.png',
-  '/manifest.json'
+  './',
+  'index.html',
+  'style.css',
+  'main.js',
+  'icon.png',
+  'manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
     })
   );
 });
